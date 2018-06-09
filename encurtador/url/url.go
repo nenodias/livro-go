4233c110e55ec6d9cab01268ad86ec300e06966a
Horácio Dias <horacio.dias92@gmail.com>
2018-06-09 17:48:13 -0300

    Atualizando o projeto do encurtador

---
+++ encurtador/url/url.go
@@ -0,0 +1,71 @@
+package url
+
+import (
+	"math/rand"
+	"net/url"
+	"time"
+)
+
+type Repositorio interface {
+	IdExiste(id string) bool
+	BuscarPorId(id string) *Url
+	BuscarPorUrl(url string) *Url
+	Salvar(url Url) error
+}
+
+func init() {
+	ConfigurarRepositorio(NovoRepositorioMemoria())
+}
+
+const (
+	tamanho  = 5
+	simbolos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-+"
+)
+
+var repo Repositorio
+
+func ConfigurarRepositorio(r Repositorio) {
+	repo = r
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+type Url struct {
+	Id      string
+	Criacao time.Time
+	Destino string
+}
+
+func BuscarOuCriarNovaUrl(destino string) (u *Url, nova bool, err error) {
+	if u := repo.BuscarPorUrl(destino); u != nil {
+		return u, false, nil
+	}
+	if _, err := url.ParseRequestURI(destino); err != nil {
+		return nil, false, err
+	}
+	url := Url{gerarId(), time.Now(), destino}
+	repo.Salvar(url)
+	return &url, true, nil
+}
+
+func gerarId() string {
+	novoId := func() string {
+		id := make([]byte, tamanho, tamanho)
+		for i := range id {
+			id[i] = simbolos[rand.Intn(len(simbolos))]
+		}
+		return string(id)
+	}
+
+	for {
+		if id := novoId(); !repo.IdExiste(id) {
+			return id
+		}
+	}
+}
+
+func Buscar(id string) *Url {
+	return repo.BuscarPorId(id)
+}

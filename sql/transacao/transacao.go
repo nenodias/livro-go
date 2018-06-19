7f63bb843dfaef6af0f3363b43122859ec0d0e5f
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2018-06-18 23:37:55 -0300

    Exemplo de insert com transação que gera um erro e o rollback é chamado

---
+++ sql/transacao/transacao.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3308)/cursogo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	tx, _ := db.Begin()
+
+	stmt, _ := tx.Prepare("insert into usuarios(id, nome) values(?,?)")
+	stmt.Exec(2000, "Bia")
+	stmt.Exec(2001, "Carlos")
+	_, err = stmt.Exec(1, "Tiago") // Chave duplicada
+	if err != nil {
+		tx.Rollback()
+		log.Fatal(err)
+	}
+	tx.Commit()
+}

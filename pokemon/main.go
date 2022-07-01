c313da619845f98f831af568c3212bf0330cecae
Horácio Dias Baptista Neto <idd_hbaptista@uolinc.com>
2022-07-01 14:37:20 -0300

    Creating a generic syncMap

--- pokemon/main.go
+++ pokemon/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"sync"
@@ -27,6 +28,22 @@ type Pokemon struct {
 	Moves     []PokemonMoveContainer `json:"moves"`
 }
 
+type GenericSyncMap[K comparable, V any] struct {
+	sync.Map
+}
+
+func (p *GenericSyncMap[K, V]) Put(key K, value V) {
+	p.Store(key, value)
+}
+
+func (p *GenericSyncMap[K, V]) Get(key K) (V, error) {
+	v, loaded := p.Load(key)
+	if !loaded {
+		return *new(V), errors.New(fmt.Sprintf("NotFoundError: key=%v", key))
+	}
+	return v.(V), nil
+}
+
 func GetPokemon(id int) Pokemon {
 	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", id)
 	res, err := http.Get(url)
@@ -42,7 +59,7 @@ func GetPokemon(id int) Pokemon {
 }
 
 func main() {
-	pokemons := sync.Map{}
+	pokemons := GenericSyncMap[int, Pokemon]{}
 	c := make(chan Pokemon)
 	for i := 1; i <= 150; i++ {
 		go func(n int) {
@@ -53,7 +70,7 @@ func main() {
 	for {
 		select {
 		case p := <-c:
-			pokemons.Store(p.Id, p)
+			pokemons.Put(p.Id, p)
 		case <-time.After(5 * time.Second):
 			done = true
 		}
@@ -62,9 +79,13 @@ func main() {
 		}
 	}
 	for i := 1; i <= 150; i++ {
-		p, _ := pokemons.Load(i)
-		pkm := p.(Pokemon)
-		fmt.Println(i, pkm.Name)
+		p, _ := pokemons.Get(i)
+		fmt.Println(i, p.Name)
 	}
+	p, err := pokemons.Get(151)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(p.Name)
 	fmt.Println("Fim")
 }
